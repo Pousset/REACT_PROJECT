5eb1cc141a0abddcdc903124c8b3917a824eaf6a
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2025-03-06 05:09:49 +0100

    add logo home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import reactLogo from '../assets/react.svg';
+
 export default function Home() {
   return (
     <div>
       <h1>Bienvenue sur mon porfolio</h1>
+      <div className="imageReact">
+        <img src={reactLogo} alt="React Logo" />
+      </div>
     </div>
   );
 }
