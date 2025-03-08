a83da18904e30ef03186be7bf48b7b09a70f4167
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2025-03-08 19:21:17 +0100

    image position + taille

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,45 @@
-import reactLogo from '../assets/react.svg';
+import reactLogo from "../assets/react.svg";
+import githubLogo from "../assets/github.svg";
+import { display } from "@mui/system";
 
 export default function Home() {
   return (
     <div>
       <h1>Bienvenue sur mon porfolio</h1>
-      <div className="imageReact">
-        <img src={reactLogo} alt="React Logo" />
+      <div style={styles.imagesContainer}>
+        <a
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.imageWrapper}
+        >
+          <img src={reactLogo} alt="React Logo" style={styles.image} />
+        </a>
+        <a
+          href="https://github.com/Pousset/REACT_PROJECT"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.imageWrapper}
+        >
+          <img src={githubLogo} alt="GitHub Logo" style={styles.image} />
+        </a>
       </div>
     </div>
   );
 }
+
+const styles = {
+  imagesContainer: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  imageWrapper: {
+    width: "80%",
+    height: "auto",
+  },
+  image: {
+    width: "100px",
+    height: "20", // Assurez-vous que l'image conserve ses proportions
+  },
+};
