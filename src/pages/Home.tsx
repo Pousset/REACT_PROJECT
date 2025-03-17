e74442aea2eb4ec6ce28884226c925daa6f5039e
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2025-03-17 19:22:58 +0100

    update home + refacto
    change disposition + suppression code inutile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,26 @@
 import reactLogo from "../assets/react.svg";
 import githubLogo from "../assets/github.svg";
 import vueLogo from "../assets/vue.svg";
+
+const styles = {
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageWrapper: {
+    margin: '0 30px',
+  },
+  image: {
+    width: '100px',
+    height: '100px',
+  },
+
+};
+
 export default function Home() {
   return (
-    <div>
+    <div className="project_presentation" style={styles.content}>
       <div style={styles.content}>
         <a
           href="https://github.com/Pousset"
@@ -13,8 +30,6 @@ export default function Home() {
         >
           <img src={githubLogo} alt="GitHub Logo" style={styles.image} />
         </a>
-        ;<h1>Bienvenue sur mon porfolio</h1>
-        <div style={styles.imagesContainer}>
           <a
             href="https://github.com/Pousset/REACT_PROJECT"
             target="_blank"
@@ -24,38 +39,14 @@ export default function Home() {
             <img src={reactLogo} alt="React Logo" style={styles.image} />
           </a>
         </div>
-        <div style={styles.imagesContainer}>
           <a
             href="https://github.com/Pousset/REACT_PROJECT"
             target="_blank"
             rel="noopener noreferrer"
             style={styles.imageWrapper}
           >
-            <img src={vueLogo} alt="React Logo" style={styles.image} />
+            <img src={vueLogo} alt="Vue Logo" style={styles.image} />
           </a>
         </div>
-      </div>
-    </div>
   );
 }
-
-const styles: { [key: string]: React.CSSProperties } = {
-  content: {
-    position: "relative",
-    zIndex: 1, // Assurez-vous que le contenu principal est au-dessus du fond étoilé
-    width: "100%",
-  },
-  imagesContainer: {
-    alignItems: "center",
-    display: "flex",
-    justifyContent: "space-around",
-  },
-  imageWrapper: {
-    width: "80%",
-    height: "auto",
-  },
-  image: {
-    width: "100px",
-    height: "20", // Assurez-vous que l'image conserve ses proportions
-  },
-};
